0493d2f8a517e5ae43610e0309264f198a02e5b4
Jurģis Brigmanis <velko.x@gmail.com>
2019-08-10 21:51:07 +0300

    Use GPIO macros for LED control

--- Tiny25-I2C-Slave/main.c
+++ Tiny25-I2C-Slave/main.c
@@ -2,8 +2,11 @@
 #include <avr/interrupt.h>
 #include <avr/eeprom.h>
 
+#include "gpio.h"
 #include "usi_twi_slave.h"
 
+
+#define LED(dir)   GPIO(B, 1, dir)
 
 #define INIT_STATE_STORE  ((uint8_t*)1)
 
@@ -12,10 +15,11 @@ int main(void)
     // Load initial state and turn output on/off
     uint8_t ledstate = eeprom_read_byte(INIT_STATE_STORE);
     if (ledstate)
-        PORTB |= 1 << PORTB1;
+        LED(PORT) = PORT_HIGH;
     else
-        PORTB &= ~(1 << PORTB1);
-    DDRB |= 1 << PORTB1;
+        LED(PORT) = PORT_LOW;
+
+    LED(DDR) = DDR_OUT;
 
     usi_twi_init(0x30);
 
@@ -37,10 +41,10 @@ int main(void)
                 if (uts_rxCnt > 1)
                 {
                     ledstate = uts_rxBuf[1];
-                    if (ledstate)
-                        PORTB |= 1 << PORTB1;
+                    if ledstate)
+                        LED(PORT) = PORT_HIGH;
                     else
-                        PORTB &= ~(1 << PORTB1);
+                        LED(PORT) = PORT_LOW;
                 }
                 uts_txBuf = ledstate;
                 break;
